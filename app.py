80cb68d4d889abdf31e3d442eddbd8227f5430e5
dexterousfm-cyber <dexterousfm@gmail.com>
2026-03-22 21:53:19 +0100

    Initial app.py file for the school management system

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
-from flask import Flask, render_template, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
+# Entry point for the school management system
+
+from flask import Flask, render_template
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///school.db'
-db = SQLAlchemy(app)
-login_manager = LoginManager(app)
 
-# Routes for authentication, students, teachers, classes, attendance, grades
-
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
